bae5b3aa4aca6c78e183c4068f4b1d7a06f1da20
Logan Natysin <lnatysin@gmail.com>
2024-05-27 17:12:11 -0400

    added basic reading and printing for the schedule

--- reader.py
+++ reader.py
@@ -1,3 +1,19 @@
 import pandas as pd
-df = pd.read_excel('Schedule.xlsx', sheet_name='Sheet1')
+from datetime import datetime
 
+# Read the Excel file
+df = pd.read_excel('Schedule.xlsx', sheet_name='Sheet1', engine='openpyxl')
+
+# Iterate through rows
+for index, row in df.iterrows():
+    if not pd.isna(row[0]):  # Check if 'Name' column is not NaN
+        if row[0] == 'CART ATTENDANTS' or row[0] == 'BEV CART ATTENDANTS' or row[0] == 'STARTERS/MARSHALS' or row[0] == 'GOLF SHOP':
+            print('Position:', row[0])
+        else:
+            print('Name:', row[0])
+            print('Phone Number:', row[1])
+            for i, column in enumerate(row[2:]):
+                if not pd.isna(column):
+                    date = df.columns[2 + i]  # Get the corresponding date from column headers
+                    formatted_date = date.strftime('%B %d, %Y')
+                    print(f"\tDate: {formatted_date}, Shift: {column}")
